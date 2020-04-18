a06cb618e5cd9647cae01f12b79df18d7b3176d6
richawil7 <62387095+richawil7@users.noreply.github.com>
2020-04-18 10:54:10 -0400

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World 3!</h1>
+    <p>This is my paragraph.</p>
+  </div>,
+  document.getElementById("root")
+);
